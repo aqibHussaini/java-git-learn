1ed4b852d9f7c0089a43e0838468f5f25273872c
aqibHussaini <67430403+aqibHussaini@users.noreply.github.com>
2021-10-17 19:33:21 +0500

    adding sum fun

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("hello world!!!");
 	System.out.println("this is my first java project with git and intelij by dev1!!");
+	sum();
+    }
+    public static void sum()
+    {
+        int a=19,b=1;
+        System.out.println(a+b);
     }
 }
