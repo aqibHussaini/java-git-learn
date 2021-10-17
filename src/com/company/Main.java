4b45ce14c11b34fabe685569ed3a6e02ab934e95
aqibHussaini <67430403+aqibHussaini@users.noreply.github.com>
2021-10-17 19:29:17 +0500

    my first sop statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("this is my first java project with git and intelij by dev1!!");
     }
 }
